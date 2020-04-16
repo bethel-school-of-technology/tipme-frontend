880c3d3aa94b277156a0085172917cff2becfdeb
Jimmy Saintel <jimmy.saintel@gmail.com>
2020-04-15 21:49:33 -0400

    Updated
    Removed App.test.js due to slight error/interference.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,6 @@ import React from 'react';
 
 import ListRestaurants from './components/restaurants';
 
-app.post('/upload', (req, res, next) => {
-  // console.log(req);
-  let imageFile = req.files.file;
-  
-  imageFile.mv(`${__dirname}/public/${req.body.filename}.jpg`, err => {
-  if (err) {
-  return res.status(500).send(err);
-  }
-  
-  res.json({ file: `public/${req.body.filename}.jpg` });
-  console.log(res.json);
-  });
-  });
-
 const App = () => (
   <div>
 
