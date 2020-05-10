917d62196198e7fb19e26c3af80f6828b9cca2bc
Kitty Cooper <46636796+Season789@users.noreply.github.com>
2020-05-09 18:22:39 -0700

    changes made to resturant.css

--- src/Screens/restaurants.js
+++ src/Screens/restaurants.js
@@ -1,45 +1,134 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+//import React from 'react';
 import Axios from 'axios';
+import Navbar from '../components/navbar'
 import '../look/css/resturants.css';
+import { render } from '@testing-library/react';
 
 
-const restaurants = [];
-
-const ListRestaurants = () => {
-  const [value, setValue] = React.useState('');
-  const [list, setList] = React.useState(restaurants);
-
-  const handleChange = event => {
-    setValue(event.target.value);
-  };
-
-  const handleSubmit = event => {
-    if (value) {
-      setList(list.concat(value));
+class ListRestaurants extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeAddress = this.onChangeAddress.bind(this);
+        this.onChangePhoneNumber = this.onChangePhoneNumber.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            Name: '',
+            Address: '',
+            PhoneNumber: '',
+            Completed: false
+        }
     }
+    onChangeName(e) {
+        this.setState({
+            Name: e.target.value
+        });
+    }
+    onChangeAddress(e) {
+        this.setState({
+            Address: e.target.value
+        });
+    }
+    onChangePhoneNumber(e) {
+        this.setState({
+            PhoneNumber: e.target.value
+        });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(`Form submitted:`);
+        console.log(`Name: ${this.state.Name}`);
+        console.log(`Address: ${this.state.Address}`);
+        console.log(`PhoneNumber: ${this.state.PhoneNumber}`);
+        this.setState({
+            Name: '',
+            Address: '',
+            PhoneNumber: '',
+            Completed: false
+        })
+    }
+    render() {
+        return (
+            <Fragment>
+                <Navbar />
+                <form onSubmit={this.onSubmit} class="container">
+                    <header><h2>Add Resturants</h2></header>
+                    <div className="form-group">
+                        <label>Name </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.Name}
+                            onChange={this.onChangeName} />
+                    </div>
 
-    setValue('');
 
-    event.preventDefault();
-  };
 
-  return (
-    <div>
+                    <div className="form-group">
+                        <label>Location </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.Address}
+                            onChange={this.onChangeAddress}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label for="phone">PhoneNumber </label>
+                        <input type="tel"
+                            placeholder="(xxx) xxx-xxxx"
+                            className="form-control"
+                            value={this.state.PhoneNumber}
+                            onChange={this.onChangePhoneNumber}
+                        />
+                    </div>
+                    <div className="form-group" >
+                        <input type="submit" value="Add Restaurant" className="btn btn-primary" />
+                    </div>
+                </form>
+            </Fragment>
+        )
+    }
+}
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={handleChange} />
-        <button type="submit">Add Restaurants</button>
-      </form>
+// const restaurants = [];
 
-      <ul>
-      {list.map(item => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
-    
-    </div>
-    
-  );
-};
+// const ListRestaurants = () => {
+//   const [value, setValue] = React.useState('');
+//   const [list, setList] = React.useState(restaurants);
+
+//   const handleChange = event => {
+//     setValue(event.target.value);
+//   };
+
+//   const handleSubmit = event => {
+//     if (value) {
+//       setList(list.concat(value));
+//     }
+
+//     setValue('');
+
+//     event.preventDefault();
+//   };
+
+//   return (
+//     <div>
+
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" value={value} onChange={handleChange} />
+//         <button type="submit">Add Restaurants</button>
+//       </form>
+
+//       <ul>
+//       {list.map(item => (
+//         <li key={item}>{item}</li>
+//       ))}
+//     </ul>
+
+//     </div>
+
+//   );
+// };
 
 export default ListRestaurants;
